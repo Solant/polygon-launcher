f2c7a21d3fd3a75a36a49de7eb5a39fd3cb6b8e7
Solant <runner62v6@gmail.com>
2020-02-04 15:05:09 +0300

    refactored buttons to use cached size/icons and functional components

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,23 @@
 import { Button as NativeButton } from "@nodegui/react-nodegui";
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
 import { QIcon, QSize } from '@nodegui/nodegui';
 
-export class Button extends React.Component<{
+export const Button: FunctionComponent<{
     clicked: () => void,
     styleSheet?: string,
     icon: string,
-}> {
-    render() {
-        return (
-            <NativeButton
-                icon={new QIcon(this.props.icon)}
-                iconSize={new QSize(40, 40)}
-                on={{ clicked: () => this.props.clicked() }}
-                styleSheet={'background-color: #1e1e1e; height: 70px; width: 90px; margin: 0px 10px 0 10px' + (this.props.styleSheet || '')}
-            />
-        );
-    }
-}
+}> = (props) => {
+    const [state] = useState({
+        size: new QSize(40, 40),
+        icon: new QIcon(props.icon),
+    });
+
+    return (
+        <NativeButton
+            icon={state.icon}
+            iconSize={state.size}
+            on={{ clicked: () => props.clicked() }}
+            style={'background-color: #1e1e1e; height: 70px; width: 90px; margin: 0px 10px 0 10px' + (props.styleSheet || '')}
+        />
+    );
+};
--- src/components/social-button.tsx
+++ src/components/social-button.tsx
@@ -1,20 +1,23 @@
 import { Button as NativeButton } from "@nodegui/react-nodegui";
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
 import { QIcon, QSize } from '@nodegui/nodegui';
 
-export class SocialButton extends React.Component<{
+export const SocialButton: FunctionComponent<{
     clicked: () => void,
     icon: string,
-}> {
-    render() {
-        return (
-            <NativeButton
-                icon={new QIcon(this.props.icon)}
-                flat={true}
-                iconSize={new QSize(40, 40)}
-                on={{ clicked: () => this.props.clicked() }}
-                styleSheet={'background-color: transparent; margin: 5px;'}
-            />
-        );
-    }
-}
+}> = (props) => {
+    const [state] = useState({
+        size: new QSize(40, 40),
+        icon: new QIcon(props.icon),
+    });
+
+    return (
+        <NativeButton
+            icon={state.icon}
+            flat={true}
+            iconSize={state.size}
+            on={{ clicked: () => props.clicked() }}
+            styleSheet={'background-color: transparent; margin: 5px;'}
+        />
+    );
+};
